8f757b1048293416d73f5281f1541635de562836
fachrulch <fachrul.fch@gmail.com>
2021-04-02 12:43:17 +0700

    fix missing function

--- functions/layout.php
+++ functions/layout.php
@@ -1,6 +1,7 @@
 <?php
 function admin_layout_sidebar()
 {
+    global $WEB_URL;
     $html = <<<HTML
     <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
@@ -8,7 +9,7 @@ function admin_layout_sidebar()
             <a href="index.html" class="brand-link">
                 <img src="dist/img/icon-sanggar-kreativitas.jpeg" alt="Sanggar Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Sanggar</span>
+                <span class="brand-text font-weight-light">Sanggar Brahmastra</span>
             </a>
 
             <!-- Sidebar -->
@@ -16,7 +17,7 @@ function admin_layout_sidebar()
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="dist/img/user7-128x128.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="$WEB_URL/images/avatar-placeholder.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">$_SESSION[name]</a>
@@ -59,12 +60,11 @@ function admin_layout_sidebar()
                                 <i class="nav-icon far fa-calendar-alt"></i>
                                 <p>
                                     Kegiatan
-                                    <span class="badge badge-info right">2</span>
                                 </p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="gallery.php" class="nav-link">
+                            <a href="galeri-karya.php" class="nav-link">
                                 <i class="nav-icon far fa-image"></i>
                                 <p>
                                     Gallery
@@ -78,6 +78,35 @@ function admin_layout_sidebar()
                                     Testimonial
                                 </p>
                             </a>
+                        </li>
+                        <li class="nav-item has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon far fa-edit"></i>
+                                <p>
+                                    Pendaftaran
+                                    <i class="fas fa-angle-left right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="pages/mailbox/mailbox.html" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Inbox</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="pages/mailbox/compose.html" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Buat</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="pages/mailbox/read-mail.html" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Read</p>
+                                    </a>
+                                </li>
+                            </ul>
                         </li>
                         <li class="nav-item has-treeview">
                             <a href="#" class="nav-link">
@@ -175,7 +204,7 @@ function admin_footer()
 
 function create_confirmation($btn_text, $btn_colour, $pesan, $target)
 {
-    $id_modal = rand(10,100);
+    $id_modal = rand(10, 100);
     $html = <<<HTML
     <!-- Trigger the modal with a button -->
     <button type="button" class="btn $btn_colour" data-toggle="modal" data-target="#myModal$id_modal">$btn_text</button>
@@ -201,4 +230,102 @@ function create_confirmation($btn_text, $btn_colour, $pesan, $target)
     </div>
     HTML;
     echo $html;
+}
+
+
+function layout_header($page_title = "Home"){
+    echo <<<HTML
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>BRAHMASTRA ART &mdash; $page_title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+        <meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+        <meta name="author" content="freehtml5.co" />
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400" rel="stylesheet" />
+
+        <!-- Animate.css -->
+        <link rel="stylesheet" href="css/animate.css" />
+        <!-- Icomoon Icon Fonts-->
+        <link rel="stylesheet" href="css/icomoon.css" />
+        <!-- Bootstrap  -->
+        <link rel="stylesheet" href="css/bootstrap.css" />
+
+        <!-- Magnific Popup -->
+        <link rel="stylesheet" href="css/magnific-popup.css" />
+
+        <!-- Owl Carousel  -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+        <!-- Flexslider  -->
+        <link rel="stylesheet" href="css/flexslider.css" />
+
+        <!-- Pricing -->
+        <link rel="stylesheet" href="css/pricing.css" />
+
+        <!-- Theme style  -->
+        <link rel="stylesheet" href="css/style.css" />
+
+        <!-- Modernizr JS -->
+        <script src="js/modernizr-2.6.2.min.js"></script>
+        <!-- FOR IE9 below -->
+        <!--[if lt IE 9]>
+        <script src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+  HTML;
+}
+
+function layout_navigation(){
+    echo <<<HTML
+    <nav class="fh5co-nav" role="navigation">
+            <div class="top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 text-right">
+                            <p class="site">brahmastra-art.com</p>
+
+                            <ul class="fh5co-social">
+                                <li>
+                                    <a href="https://facebook.com"><i class="icon-facebook2"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com"><i class="icon-twitter2"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/firahpp"><i class="icon-github"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="top-menu">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <div id="fh5co-logo">
+                                <a href="index.php"><i class="icon-palette"></i>Brahmastra Art<span>.</span></a>
+                            </div>
+                        </div>
+                        <div class="col-xs-10 text-right menu-1">
+                            <ul>
+                                <li class="active"><a href="index.php">Home</a></li>
+                                <li><a href="kelas.html">Kelas</a></li>
+                                <li><a href="guru.php">Guru</a></li>
+                                <li><a href="kegiatan.php">Kegiatan</a></li>
+                                <li><a href="gallery.php">Gallery</a></li>
+                                <li class="btn-cta"><a href="contact.html"><span>Daftar</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    HTML;
 }
